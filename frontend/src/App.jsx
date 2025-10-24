fbbef54df6e27f2035755cead12dadd7902d4bee
Nga3006 <ngatnp8468@ut.edu.vn>
2025-10-24 15:32:52 +0700

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
-import AppRouter from './routes/AppRouter';
-import './styles/global.css'; // Import file CSS global
-import MapPage from "./pages/MapPage";
-
+import React from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import HomePage from "./components/HomePage";
+// hoặc MapPage nếu bạn muốn hiện bản đồ
+// import MapPage from "./components/MapPage";
+import "./styles/global.css";
+import "./styles/header.css";
+import "./styles/footer.css";
+import "./styles/homepage.css";
 
 function App() {
-    return (
-        <div className="App">
-            <MapPage />
-            <AppRouter />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <HomePage />
+        {/* <MapPage /> nếu bạn muốn hiện trang bản đồ thay vì homepage */}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
